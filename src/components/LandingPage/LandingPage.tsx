232c095be8cf24ab27603f1be309f173902f8378
Tyler Tran <tylerduytran@gmail.com>
2023-06-28 00:07:34 -0700

    Revised Navigation Bar & Landing Page
    Added a temporary gradient to the background of the landing page, changed the fonts on the text. Centered the elements on the landing page and added an image that I may or may not keep on that landing page. I think I am right now leaning towards moving that image to the about section instead. I also have tried a few implementations of adding transitions/animations to the fonts of my landing page but have not been too successful: namely I used react-reveal and react-awesome-reveal. I also had trouble with react-text-transition. This is still in the works and is something I will get to tommorow. I am hoping to finish this navbar and landing page tomorrow as well as the About Page tomorrow.
    
    Current Sprint Plan for the week is as follows:
    Wednesday - 6/28
    - Styling of Navbar, Landing Page & About Page
    Thursday - 6/29
    - Styling of Experience, Projects Page
    Friday - 6/30
    - Mobile Screen sizing adjustments, deployment

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './LandingPage.scss';
+// import { Fade } from "react-awesome-reveal";
+// import TextTransition, { presets } from 'react-text-transition';
+
+import  ProfilePhoto  from '../../images/Profile-Photo.jpeg';
 
 export default function LandingPage(){
     return (
-      <div className="landing-inner-container h-screen">
-          <h1 className='Landing-Name'>Tyler Tran</h1>
-          <h2 className='Landing-Position'>Software Engineer</h2>
+      <div className="landing-inner-container flex flex-col items-center justify-center h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
+            {/* <Fade direction="up" triggerOnce={true}> */}
+            
+                <h1 className='Landing-Name text-white'>Tyler Tran</h1>
+            
+            {/* </Fade> */}
+            {/* <Fade direction="up" triggerOnce={true} delay={500}> */}
+                <h2 className='Landing-Position'>Software Engineer</h2>
+            {/* </Fade> */}
+          <img className='Landing-Image' src={ProfilePhoto} alt='Tyler Tran' />
       </div>
     );
 }
